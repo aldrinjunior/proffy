4d75b4ef8f52a118ccb15e68cd837486835494c8
aldrin junior <aldrinjunior@gmail.com>
2020-08-06 00:32:41 -0300

    landing-page-pronta

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function TeacherForm() {
+  return <h1>TeacherForm</h1>;
+}
+
+export default TeacherForm;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function TeacherList() {
+  return <h1>Teachers</h1>;
+}
+
+export default TeacherList;
