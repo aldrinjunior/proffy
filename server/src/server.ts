297173d9ac740fd04cc0eace7ee453a437b72131
aldrin junior <aldrinjunior@gmail.com>
2020-08-08 19:03:53 -0300

    backend criado2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,11 @@
 import express from "express";
 
 const app = express();
-//recurso da rota se chama users, agora o require é o que o front informa e response é a resposta do retorno do backend
-app.get("/users", (require, response) => {
-  const users = [
-    { name: "aldrin", age: "20" },
-    { name: "fulano", age: "20" },
-  ];
-  return response.json(users);
-});
 
-//localhost:3333/users
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
 
 app.listen(3333);
